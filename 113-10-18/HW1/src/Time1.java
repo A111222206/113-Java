493bd7568dde6f465293e340615c217def74a246
A111222206 <168390844+A111222206@users.noreply.github.com>
2024-10-18 12:01:32 +0800

    113-10-18

---
+++ 113-10-18/HW1/src/Time1.java
@@ -0,0 +1,13 @@
+public class Time1 {
+    //時hour 分minute 秒second
+
+    //方法: 建構子(設定時分秒初始值 需檢查資料是否正確)
+
+    //方法: 加入get/set
+
+    //方法: tick(時間增加一秒)
+
+    //方法: printUniversal(以24小時制輸出完整時間 00:01:00)
+
+    //方法: printStandard(以12小時制輸出完整時間 12:01:00AM)
+}
